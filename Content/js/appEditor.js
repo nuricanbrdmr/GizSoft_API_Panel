f212fc5a0f85afa2554d5a962569a340aa098598
nuricanbrdmr <nuricanb6@gmail.com>
2023-08-08 09:26:54 +0300

    Modalda güncelleme
    File -> New Modalında proje seçme, seçim yapıp ilerledikten sonra proje detay girme tasarımı ve dinamikliği sağlandı.

--- Content/js/appEditor.js
+++ Content/js/appEditor.js
@@ -1,13 +1,115 @@
 ﻿var app = angular.module('app', ['ngRoute', 'ngSanitize', 'ngMaterial', 'ngMessages', 'ngAnimate']);
 
-app.controller('myController', function ($scope) {   
-
+app.controller('myController', function ($scope, $sce) {   
+    //Modal Code
     $scope.openModal = function () {
         $('#myModal').modal('show');
     };
-    $scope.closeModal = function () {
-        $('#myModal').modal('hide');
+    $scope.closeModal = function (buton) {
+        if (buton == "myModal") {
+            $('#myModal').modal('hide');
+        }
+        $('#configureModal').modal('hide');
+        $scope.selectedProject = null;
+        $scope.selectedFavType = null;
     };
+    $scope.openDifferentModal = function () {
+        $('#myModal').modal('hide'); // İlk modalı gizle
+        $('#configureModal').modal('show'); // Farklı modalı göster
+        if ($scope.selectedProjectFav) {
+            $scope.isNextButtonEnabled = true;
+            $scope.selectedContentHeader = {
+                title: $scope.selectedProjectFav.favName,
+                platform: $scope.selectedProjectFav.favtype
+            };
+        }
+    };
+    $scope.backModal = function () {
+        $('#configureModal').modal('hide'); // İlk modalı gizle
+        $('#myModal').modal('show'); // Farklı modalı göster
+    }
+
+    $scope.selectedProjectFav = null;
+    $scope.isNextButtonEnabled = false; // Başlangıçta butonu devre dışı bırakın.
+    $scope.ClickFavItem = function (favItem) {
+        // tıklanan öğenin işlemlerini burada yapabilirsiniz.
+        $scope.selectedFavItem = favItem; // tıklanan öğeyi seçili öğe olarak işaretlemek istiyorsanız.
+        $scope.isNextButtonEnabled = true; // "Next" butonunu etkinleştirin.
+        $scope.selectedProjectFav = favItem;
+    };
+
+
+    $scope.projectType = [
+        {
+            icon: $sce.trustAsHtml("<i class='bx bxl-typescript'></i>"),
+            name: "Type Script",
+            description: "TypeScript allows specifying the types of data being passed around within the code, and has the ability to report errors when the types don't match.",
+            platforms: ["C#", "Js", "Web"],
+            favTypeList: [
+                { favIcon: "<i class='bx bx-code-block'></i>" , favName: "Console App", favtype: "C"}
+            ]
+        },
+        {
+            icon: "<i class='bx bx-code-block'></i>",
+            name: "Console App",
+            description: "A project for creating a command-line application that can run on .NET on Windows, Linux and macOS.",
+            platforms: ["C#", "ASP.NET", "Web"],
+            favTypeList: [
+                { favIcon: "<i class='bx bx-code-block'></i>", favName: "Console App", favtype: "C" },
+                { favIcon: "<i class='bx bxl-typescript'></i>", favName: "Type Script App", favtype: "Ty" }
+
+            ]
+        },
+        {
+            icon: "<i class='bx bxl-typescript'></i>",
+            name: "Type Script",
+            description: "TypeScript allows specifying the types of data being passed around within the code, and has the ability to report errors when the types don't match.",
+            platforms: ["C#", "Js", "Web"],
+            favTypeList: [
+                { favIcon: "<i class='bx bx-code-block'></i>", favName: "Console App", favtype: "C" }
+            ]
+        },
+        {
+            icon: "<i class='bx bx-code-block'></i>",
+            name: "Console App",
+            description: "A project for creating a command-line application that can run on .NET on Windows, Linux and macOS.",
+            platforms: ["C#", "ASP.NET", "Web"],
+            favTypeList: [
+                { favIcon: "<i class='bx bx-code-block'></i>", favName: "Console App", favtype: "C" }
+            ]
+        },
+        {
+            icon: "<i class='bx bxl-typescript'></i>",
+            name: "Type Script",
+            description: "TypeScript allows specifying the types of data being passed around within the code, and has the ability to report errors when the types don't match.",
+            platforms: ["C#", "Js", "Web"],
+            favTypeList: [
+                { favIcon: "<i class='bx bx-code-block'></i>", favName: "Console App", favtype: "C" },
+                { favIcon: "<i class='bx bxl-typescript'></i>", favName: "Type Script App", favtype: "C" }
+            ]
+        }
+    ];
+
+    $scope.selectedProject = null;
+    $scope.selectedFavType = [];
+    $scope.trustHtml = function (html) {
+        return $sce.trustAsHtml(html);
+    };
+    $scope.showFavType = function (item) {
+        if ($scope.selectedProject != null) {
+            $scope.selectedProject = item;
+            $scope.selectedProjectFav = null;
+            $scope.isNextButtonEnabled = false; // "Next" butonunu etkinleştirin.
+        } else {
+            $scope.selectedProject = item;
+
+        }
+        $scope.selectedFavType = item.favTypeList;
+    };
+
+   
+
+    // Modal End Code
 
     $scope.contentsItems = [
         { name: 'Web Service', content: 'Order', isActive: false, contents: [], orderItems: [] },
